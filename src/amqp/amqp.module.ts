df175c1ba12f54b220c1bc713e1a530a4560d22b
vladhuk <vladhookovskiy@gmail.com>
2021-08-28 02:09:55 +0300

    - init project;

---
+++ src/amqp/amqp.module.ts
@@ -0,0 +1,35 @@
+import { AmqpConnection, RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
+import { Global, Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { AmqpService } from './amqp.service';
+
+@Global()
+@Module({
+  imports: [
+    RabbitMQModule.forRootAsync(RabbitMQModule, {
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get('AMQP_URL'),
+        exchanges: [
+          {
+            name: 'price',
+            type: 'topic',
+          },
+          {
+            name: 'profile',
+            type: 'topic',
+          },
+        ],
+        connectionInitOptions: { wait: false },
+      }),
+    }),
+  ],
+  providers: [
+    {
+      provide: AmqpService,
+      useExisting: AmqpConnection,
+    },
+  ],
+  exports: [AmqpService],
+})
+export class AmqpModule {}
