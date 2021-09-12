c9a149d3a53d7f25e2ed97fda25425b04038e933
vladhuk <vladhookovskiy@gmail.com>
2021-09-12 03:32:51 +0300

    - add decorator;

---
+++ src/_common/decorators/on-behalf-of.decorator.ts
@@ -0,0 +1,15 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+
+/**
+ * Returns user id on whose behalf the request is being made
+ *
+ * @example
+ * .@Get()
+ * async find(@OnBehalfOf() userId: string) {}
+ */
+export const OnBehalfOf = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    return request.headers['on-behalf-of'];
+  }
+);
