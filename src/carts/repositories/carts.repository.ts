097c7bbe59dd6d3b03147e562f8abc73eec042d2
vladhuk <vladhookovskiy@gmail.com>
2021-09-28 01:59:17 +0300

    - refactor query;

--- src/carts/repositories/carts.repository.ts
+++ src/carts/repositories/carts.repository.ts
@@ -1,24 +1,19 @@
-import { isDefined } from 'class-validator';
-import { EntityRepository, Repository } from 'typeorm';
+import { EntityRepository, Repository, SelectQueryBuilder } from 'typeorm';
 import { CartItemStatus } from '../entities/cart-item.entity';
 import { Cart } from '../entities/cart.entity';
 
 @EntityRepository(Cart)
 export class CartsRepository extends Repository<Cart> {
   findAllWithItemsByItemsStatus(itemsStatus?: CartItemStatus): Promise<Cart[]> {
-    const qb = this.createQueryBuilder('cart')
-      .leftJoinAndSelect('cart.items', 'items')
-      .leftJoinAndSelect('items.customer', 'itemCustomer')
-      .leftJoinAndSelect('items.product', 'itemsProduct')
-      .leftJoinAndSelect('itemsProduct.store', 'itemsProductStore')
-      .leftJoinAndSelect('cart.owner', 'owner')
-      .leftJoinAndSelect('cart.store', 'store')
-      .where('cart.isClosed = false');
+    return this.find({
+      where: (qb: SelectQueryBuilder<Cart>) => {
+        qb.where({ isClosed: false });
 
-    if (isDefined(itemsStatus)) {
-      qb.andWhere('items.status = :itemsStatus', { itemsStatus });
-    }
-
-    return qb.getMany();
+        if (itemsStatus !== undefined) {
+          qb.andWhere('Cart__items.status = :itemsStatus', { itemsStatus });
+        }
+      },
+      relations: ['items'],
+    });
   }
 }
